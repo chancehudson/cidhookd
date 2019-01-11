5176924436ca2c46499f7b37bea35a4f2161a1ee
Chance Hudson <jchancehud@gmail.com>
2019-01-11 05:45:48 -0600

    Handle output better, remove stray console.log

--- cmd.js
+++ cmd.js
@@ -4,10 +4,12 @@ const fetch = require('node-fetch');
 const [ _, __, _domain, cid, method ] = process.argv;
 
 const domain = _domain.indexOf('http') === 0 ? _domain : `https://${_domain}`;
-console.log(`${domain}/${cid}`);
 
 fetch(`${domain}/${cid}`, {
   method: method || 'POST'
 })
-  .then(res => console.log(res))
-  .catch(err => console.log('err', err));
+  .then(res => {
+    if (res.status !== 204) throw new Error('Non-204 response received');
+    console.log(`cid ${cid} successfully ${method === 'DELETE' ? 'un-' : ''}hooked`)
+  })
+  .catch(err => console.log('Error', err));
