130e3e53bc9a4573b3639277688477bdfc50bf09
Chance Hudson <jchancehud@gmail.com>
2019-01-11 03:07:04 -0600

    Refactor express server handlers

--- index.js
+++ index.js
@@ -4,31 +4,29 @@ const app = express();
 const IPFS = require('ipfs');
 const node = new IPFS();
 
-app.get('/:cid', handler);
-
-async function handler(req, res) {
+app.post('/:cid', async (req, res) => {
   try {
-    if (req.method === 'POST') {
-      // Pin the path
-      await node.pin.add(req.params.cid, {
-        recursive: true
-      });
-      console.log(`Pinned cid ${req.params.cid}`);
-      res.status(204).end();
-    } else if (req.method === 'DELETE') {
-      // Remove the pin
-      await node.pin.rm(req.params.cid, {
-        recursive: true
-      });
-      console.log(`Unpinned cid ${req.params.cid}`);
-      res.status(204).end();
-    } else {
-      res.status(404).end();
-    }
+    // Pin the path
+    await node.pin.add(req.params.cid, {
+      recursive: true
+    });
+    console.log(`Pinned cid ${req.params.cid}`);
+    res.status(204).end();
   } catch (err) {
     res.end(err);
   }
-}
+});
+app.delete('/:cid', async (req, res) => {
+  try {
+    await node.pin.rm(req.params.cid, {
+      recursive: true
+    });
+    console.log(`Unpinned cid ${req.params.cid}`);
+    res.status(204).end();
+  } catch (err) {
+    res.end(err);
+  }
+});
 
 node.on('ready', () => {
   app.listen(3000, () => console.log(`cidhook listening on port 3000!`));
